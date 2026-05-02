d9f11d84b9fb300fba14a9032a793632b1b7f4ca
daxiong <1173238834@qq.com>
2026-05-02 14:56:34 +0800

    ai-server初始化提交v2

--- ski-ai-server/app/config.py
+++ ski-ai-server/app/config.py
@@ -18,7 +18,7 @@ class Settings(BaseSettings):
 
     # ----------- 服务配置 -----------
     host: str = "0.0.0.0"
-    port: int = 8000
+    port: int = 8001
     log_level: str = "INFO"
 
     # 视频存储路径(安全沙箱)
